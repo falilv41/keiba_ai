ae4d853cd2a3deede5c039bf38d5bd90eca84501
falilv41 <falilv41@gmail.com>
2024-07-19 17:15:03 +0900

    second_impact

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ from pathlib import Path
 
 
 st.subheader("予測：7月20日")
-df = pd.read_csv("data/pred_df.csv")
-df = df[["買い目","odds_over","tansho_odds","pred","kitaiti"]]
-st.dataframe(df)
+df_sat = pd.read_csv("data/pred_df_0720.csv")
+df_sat = df_sat[["買い目","odds_over","tansho_odds","pred","kitaiti"]]
+st.dataframe(df_sat)
+
+
+st.subheader("予測：7月21日")
+df_sun = pd.read_csv("data/pred_df_0721.csv")
+df_sun = df_sun[["買い目","horse_name","odds_over","tansho_odds","pred","kitaiti"]]
+st.dataframe(df_sun)
+
+
+
+
