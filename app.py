75b1c412e8a989be2ad61ae051dfcdb5b1cb7d07
falilv41 <falilv41@gmail.com>
2024-07-20 10:06:29 +0900

    0720

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from pathlib import Path
 
 
 
-st.subheader("予測：7月20日")
-df_sat = pd.read_csv("data/pred_df_0720.csv")
-df_sat = df_sat[["買い目","odds_over","tansho_odds","pred","kitaiti"]]
+st.subheader("予測：土曜日")
+df_sat = pd.read_csv("data/pred_df_sat.csv")
+df_sat = df_sat[["買い目","horse_name","odds_over","tansho_odds","pred","kitaiti"]]
 st.dataframe(df_sat)
 
 
-st.subheader("予測：7月21日")
-df_sun = pd.read_csv("data/pred_df_0721.csv")
+st.subheader("予測：日曜日")
+df_sun = pd.read_csv("data/pred_df_sun.csv")
 df_sun = df_sun[["買い目","horse_name","odds_over","tansho_odds","pred","kitaiti"]]
 st.dataframe(df_sun)
 
